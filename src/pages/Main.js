e3c43d2d785838ae17ca5be4a2c82cbb67e92f90
Rodrigo Medeiros <rmedeiros@techsocial.com.br>
2019-11-29 21:42:13 -0300

    show date limit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,19 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { ListGroup } from 'react-bootstrap';
 import { LinkContainer as Link } from 'react-router-bootstrap'
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 import logo from '../assets/medeirostec_logo.png'
 import { getAuth } from '../services/auth';
 import Navbar from '../components/Navbar'
 
 export default function Main ({ history }) {
+    moment.locale('pt-br');
     const [name, setName] = useState('');
+    const [dateLimit, setDateLimit] = useState('');
 
     useEffect(
         () => {
             const auth = getAuth();
             if (auth) {
                 setName(auth.user.name);
+                setDateLimit(auth.user.date_limit);
             }
         }
     , [])
@@ -27,7 +32,8 @@ export default function Main ({ history }) {
             <div className="row" style={{ marginTop: 50 }}>
                 <div className="col-md-4 offset-md-4">
                     <p className="text-center"><img src={ logo } alt="MedeirosTEC" /></p>
-                    <p className="text-center">Bem vindo <b>{ name }</b>!</p>
+                    <p className="text-center">Bem vindo <b>{name}</b>!</p>
+                        {dateLimit && (<p className="text-center">Sua assinatura vence {moment(dateLimit).fromNow()} ({moment(dateLimit).format('DD/MM/YYYY')})</p>)}
 
                     <ListGroup>
                         <Link to="/customers">
